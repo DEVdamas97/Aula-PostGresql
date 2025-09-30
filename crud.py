75d9c5f2243750e4fdae128cff51a0c57d6ce6cf
Vinicius Damas <viniciusdamas97@gmail.com>
2025-09-30 11:19:54 -0300

    Função atualizar aluno ✔

--- crud.py
+++ crud.py
@@ -36,5 +36,18 @@ def listar_alunos():
             cursor.close()
             conexao.close()
 
-            
+def atualizar_aluno(id_aluno, nova_idade):
+    conexao, cursor = conectar()
+    if conexao:
+        try:
+            cursor.execute(
+            "UPDATE alunos SET idade = %s WHERE id = %s",
+            (nova_idade, id)
+            )
+        except Exception as erro:
+            print(f"Erro ao atualizar um aluno {erro}")
+        finally:
+            cursor.close()
+            conexao.close()
+
 
